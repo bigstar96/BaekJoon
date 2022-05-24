25f57a35fa603001154a94b7c1b84f543e75c17f
KT HAN <bigstarone96@gmail.com>
2022-05-25 05:01:51 +0900

    5개?

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	int t;
+	std::cin >> t;
+
+	int n = t;
+
+	for (int i = 1; i <= t; i++)
+	{
+		for (int j = 1; j < n; j++)
+		{
+			std::cout << " ";
+		}
+		for (int j = 1; j <= i; j++)
+		{
+			std::cout << "*";
+		}
+		n--;
+		std::cout << std::endl;
+	}
 }
