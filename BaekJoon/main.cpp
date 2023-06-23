771c6d68510057328320c9c3619a7d41f02fbbfc
bigstar One <bigstarone96@gmail.com>
2023-06-24 04:03:49 +0900

    2839 설탕 배달

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,17 +1,61 @@
+//상근이는 요즘 설탕공장에서 설탕을 배달하고 있다.
+// 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다.
+// 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다.봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
+//
+//상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다.
+// 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 
+// 3킬로그램 봉지 6개를 가져가도 되지만, 
+// 5킬로그램 3개와 3킬로그램 1개를 배달하면, 
+// 더 적은 개수의 봉지를 배달할 수 있다.
+//
+//상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 
+// 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
+
 #include <iostream>
 
 int main()
 {
-	unsigned int N{};
-	std::cin >> N;
-	int min{1000000}, max{-1000000};
-	int input{};
-	for (int i = 0; i < N; ++i)
-	{
-		std::cin >> input;
-		if (min > input) min = input;
+	int num{};
+	std::cin >> num;
 
-		if (max < input) max = input;
+	if (3 <= num && num <= 5000)
+	{
+		int a3{}, b5{};
+		int count{};
+		while (num > 0)
+		{
+			if (num % 5 == 0)
+			{
+				num -= 5;
+				b5++;
+			}
+			else if (num % 3 == 0)
+			{
+				num -= 3;
+				a3++;
+			}
+			else if ((num - (count * 5)) % 3 < 0 || (num - (count * 3)) % 5 < 0)
+			{
+				a3 = 0;
+				b5 = -1;
+				break;
+			}
+			else if ((num - (count * 5)) % 3 == 0)
+			{
+				num -= 3;
+				a3++;
+			}
+			else if ((num - (count * 3)) % 5 == 0)
+			{
+				num -= 5;
+				b5++;
+			}
+			else
+			{
+				count++;
+			}
+
+		}
+		std::cout << a3 + b5;
 	}
-	std::cout << min << " " << max;
 }
