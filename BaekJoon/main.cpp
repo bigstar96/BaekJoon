e2e2943825a4ab47ef007c93b5e7a84f22e42b5f
bigstar One <bigstarone96@gmail.com>
2023-07-01 23:52:40 +0900

    5585 거스름돈

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,62 +1,30 @@
-//옛날 옛적에 수학이 항상 큰 골칫거리였던 나라가 있었다.
-// 이 나라의 국왕 김지민은 다음과 같은 문제를 내고 큰 상금을 걸었다.
-//
-//길이가 N인 정수 배열 A와 B가 있다.다음과 같이 함수 S를 정의하자.
-//
-//S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자.단, B에 있는 수는 재배열하면 안 된다.
-//
-//S의 최솟값을 출력하는 프로그램을 작성하시오.
-//
-//첫째 줄에 N이 주어진다.둘째 줄에는 A에 있는 N개의 수가 순서대로 주어지고, 
-// 셋째 줄에는 B에 있는 수가 순서대로 주어진다.N은 50보다 작거나 같은 자연수이고, 
-// A와 B의 각 원소는 100보다 작거나 같은 음이 아닌 정수이다.
-//
+
 
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 
 int main()
 {
-	int num;
+	int arr[6]{ 500,100,50,10,5,1 };
+	int num{};
 	std::cin >> num;
+	int count{}, i{};
 
-	std::vector<int> A;
-	for (int i = 0; i < num; ++i)
+	num = 1000 - num;
+
+	while (num != 0)
 	{
-		int a;
-		std::cin >> a;
-		A.push_back(a);
-	}
-	std::vector<int> B;
-	for (int i = 0; i < num; ++i)
-	{
-		int a;
-		std::cin >> a;
-		B.push_back(a);
-	}
-
-	std::sort(A.begin(), A.end());
-
-	std::vector<bool> chk(num);
-	int sum{};
-
-	for (int i = 0; i < num; ++i)
-	{
-		int temp = 0;
-		int idx = 0;
-		for (int j = 0; j < num; ++j)
+		if (num >= arr[i])
 		{
-			if (temp < B[j] && chk[j] == 0)
-			{
-				temp = B[j];
-				idx = j;
-			}
+			num -= arr[i];
+			count++;
 		}
-		chk[idx] = 1;
-		sum += A[i] * temp;
+		else
+		{
+			i++;
+		}
 	}
 
-	std::cout << sum;
+	std::cout << count;
 }
